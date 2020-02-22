b6904fcc589517cc9c3b48678b5fcd7161f2c906
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-22 13:02:21 -0800

    in28 minutes java basics

---
+++ java-basics-in28minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -0,0 +1,7 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTable {
+    public static void main(String args[]) {
+        System.out.println("Hello in 28 Minutes");
+    }
+}
