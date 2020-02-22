4ce0060522e68d4f6964d32e370572419e8883ad
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-22 13:05:52 -0800

    in28 minutes java basics

--- java-basics-in28minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTable.java
+++ java-basics-in28minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -1,7 +1,18 @@
 package com.in28minutes.firstjavaproject;
 
 public class MultiplicationTable {
+
+    public void print() {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
+        }
+    }
+
+
     public static void main(String args[]) {
         System.out.println("Hello in 28 Minutes");
+
+        MultiplicationTable table = new MultiplicationTable();
+        table.print();
     }
 }
