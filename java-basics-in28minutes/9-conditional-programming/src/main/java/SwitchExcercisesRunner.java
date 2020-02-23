67cadf89cbc9afe60319ff8a6abf077d0c625e41
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-23 10:24:25 -0800

    added code

---
+++ java-basics-in28minutes/9-conditional-programming/src/main/java/SwitchExcercisesRunner.java
@@ -0,0 +1,38 @@
+public class SwitchExcercisesRunner {
+    public static void main(String[] args) {
+        System.out.println(determineNameOfDay(2));
+
+    }
+
+    public static String determineNameOfDay(int dayNumber) {
+
+        switch (dayNumber) {
+            case 0:
+                return "Sunday";
+
+            case 1:
+                return "Monday";
+
+            case 2:
+                return "Tuesday";
+
+            case 3:
+                return "Wednesday";
+
+            case 4:
+                return "Thursday";
+
+            case 5:
+                return "Friday";
+
+            case 6:
+                return "Saturday";
+
+            default:
+                return "Invalid number passed";
+
+        }
+
+
+    }
+}
