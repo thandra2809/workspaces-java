2c86079f358398c7d17c2de63481613b42668a1b
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-23 08:12:17 -0800

    added modules

---
+++ java-basics-in28minutes/9-conditional-programming/src/main/java/IfElseIfStatementRunner.java
@@ -0,0 +1,16 @@
+public class IfElseIfStatementRunner {
+    public static void main(String[] args) {
+
+        int i = 27;
+
+        if (i == 25) {
+            System.out.println("i =  " + i);
+        } else if (i == 26) {
+            System.out.println("i = " + i);
+        } else if (i == 27) {
+            System.out.println("i = " + i);
+        } else {
+            System.out.println("i != 25 or 26 or 27 It is " + i);
+        }
+    }
+}
---
+++ java-basics-in28minutes/9-conditional-programming/src/main/java/IfStatementRunner.java
@@ -0,0 +1,13 @@
+public class IfStatementRunner {
+
+    public static void main(String args[]) {
+        int i = 26;
+        if (i == 25) {
+            System.out.println("i=25");
+        } else {
+            System.out.println("i!=25");
+
+        }
+    }
+
+}
