24443833fdb9f420a468a32fd05359a8ec8d1628
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-26 00:28:35 -0800

    added code

---
+++ java-basics-in28minutes/14-collections/src/main/java/StudentCollectionRunner.java
@@ -0,0 +1,23 @@
+import com.sun.tools.javac.util.List;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class StudentCollectionRunner {
+    public static void main(String[] args) {
+        List<Student> students = List.of(
+                new Student(1, "Srini"),
+                new Student(4, "Shreyus"),
+                new Student(3, "Priya"),
+                new Student(2, "Lakshmi"));
+
+        ArrayList<Student> studentAl = new ArrayList<>(students);
+
+        System.out.println(studentAl);
+
+        Collections.sort(studentAl);
+        System.out.println(studentAl);
+
+    }
+}
