68fd096c436b041861bbe6a7336e393eccc09a2d
Srinivasa Rao THANDRA <thandra2809>
2021-03-01 22:41:02 -0800

    Added some small changes

---
+++ gradle-demo/src/main/java/TestMain.java
@@ -0,0 +1,5 @@
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("This is my play with Gradle");
+    }
+}
---
+++ gradle-demo/src/main/java/TestMainNew.java
@@ -0,0 +1,5 @@
+public class TestMainNew {
+    public static void main(String[] args) {
+        System.out.println("This is my play with Gradle");
+    }
+}
